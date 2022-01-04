30ebf7e4fef837e5aeaf611f9a4b756d3df33411
Dunc4n <76918336+Dunc4nNT@users.noreply.github.com>
2022-01-04 13:27:02 +0100

    add day 1

---
+++ src/bin/day01/main.rs
@@ -0,0 +1,37 @@
+use itertools::Itertools;
+
+fn get_numbers(input: &str) -> Vec<i32> {
+    input.lines()
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+fn part1(input: &str) -> i32 {
+    let numbers: Vec<i32> = get_numbers(input);
+
+    for pair in numbers.iter().combinations(2) {
+        if pair[0] + pair[1] == 2020 {
+            return pair[0] * pair[1];
+        }
+    }
+    return 0;
+}
+
+
+fn part2(input: &str) -> i32 {
+    let numbers: Vec<i32> = get_numbers(input);
+
+    for pair in numbers.iter().combinations(3) {
+        if pair[0] + pair[1] + pair[2] == 2020 {
+            return pair[0] * pair[1] * pair[2];
+        }
+    }
+    return 0;
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Day 1, Part 1: {:?}", part1(input));
+    println!("Day 1, Part 2: {:?}", part2(input));
+}
