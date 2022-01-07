4a96f2373c2174398306ef96f9a770c97bf1f2b6
Dunc4n <76918336+Dunc4nNT@users.noreply.github.com>
2022-01-07 16:15:47 +0100

    add day 9

---
+++ src/bin/day09/main.rs
@@ -0,0 +1,74 @@
+use itertools::Itertools;
+
+fn process_data(input: &str) -> Vec<i64> {
+    input.lines()
+    .filter_map(|s| s.parse().ok())
+    .collect()
+}
+
+fn check_sum(numbers: Vec<i64>, num: i64) -> bool {
+    for pair in numbers.iter().combinations(2) {
+        if pair[0] + pair[1] == num {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn get_bad_num(data: Vec<i64>, size: usize) -> i64 {
+    let mut bad_num: i64 = 0;
+    let mut last_25: Vec<i64> = Vec::new();
+
+    for i in 0..size {
+        last_25.push(data[i]);
+    }
+
+    let start: usize = size;
+    for i in start..data.len() {
+        if check_sum(last_25.clone(), data[i]) {
+            last_25.remove(0);
+            last_25.push(data[i]);
+        } else {
+            bad_num = data[i];
+            break;
+        }
+    }
+
+    return bad_num;
+}
+
+fn part1(input: &str, size: usize) -> i64 {
+    let data: Vec<i64> = process_data(input);
+    let bad_num = get_bad_num(data, size);
+
+    return bad_num;
+}
+
+
+fn part2(input: &str, size: usize) -> i64 {
+    let data: Vec<i64> = process_data(input);
+    let bad_num = get_bad_num(data.clone(), size);
+    let mut result: i64 = 0;
+
+    for i in 3..size {
+        for window in data.windows(i) {
+            let sum: i64 = window.iter().sum();
+            if sum == bad_num {
+                let min = window.iter().min().unwrap();
+                let max = window.iter().max().unwrap();
+                result = min + max;
+                return result;
+            }
+        }
+    }
+
+    return result;
+}
+
+fn main() {
+    let input: &str = include_str!("input.txt");
+    let size: usize = 25;
+
+    println!("Day 9, Part 1: {:?}", part1(input, size));
+    println!("Day 9, Part 2: {:?}", part2(input, size));
+}
