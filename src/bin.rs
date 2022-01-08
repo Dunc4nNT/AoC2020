6ebbdfa6a35dc12d8741b4861debbe30d0289521
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2022-01-08 19:51:29 +0100

    improve script

--- src/bin.rs
+++ src/bin.rs
@@ -8,6 +8,21 @@ pub mod day07;
 pub mod day08;
 pub mod day09;
 pub mod day10;
+// pub mod day11;
+// pub mod day12;
+// pub mod day13;
+// pub mod day14;
+// pub mod day15;
+// pub mod day16;
+// pub mod day17;
+// pub mod day18;
+// pub mod day19;
+// pub mod day20;
+// pub mod day21;
+// pub mod day22;
+// pub mod day23;
+// pub mod day24;
+// pub mod day25;
 
 pub fn solve_all() {
     day01::solve::solve();
@@ -20,4 +35,19 @@ pub fn solve_all() {
     day08::solve::solve();
     day09::solve::solve();
     day10::solve::solve();
+    // day11::solve::solve();
+    // day12::solve::solve();
+    // day13::solve::solve();
+    // day14::solve::solve();
+    // day15::solve::solve();
+    // day16::solve::solve();
+    // day17::solve::solve();
+    // day18::solve::solve();
+    // day19::solve::solve();
+    // day20::solve::solve();
+    // day21::solve::solve();
+    // day22::solve::solve();
+    // day23::solve::solve();
+    // day24::solve::solve();
+    // day25::solve::solve();
 }
